ee8829fc3b23a1ced7cf420fcd4ee70b1e51471f
pradeepcrush <spradeepsmart@gmail.com>
2017-04-19 15:30:38 +0530

    Add files via upload

---
+++ program3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+char x;
+printf("enter the char");
+scanf("%c",x);
+if(x=="a"||x=="e"||x=="i"||x=="o"||x=="u"||x=="A"||x=="E"||x=="I"
+   ||x=="O"||x=="U")
+   printf("x is vowels");
+   else
+    printf("x is constant");
+
+}

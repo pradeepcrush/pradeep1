33989f80ded2097110d7893114c192e29f4cf288
pradeepcrush <spradeepsmart@gmail.com>
2017-06-06 16:00:18 +0530

    Add files via upload

---
+++ pradeep4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+printf("enter the char");
+scanf("%d",c);
+if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+printf("It is alphabet");
+else
+printf("It is not alphabet);
+}
